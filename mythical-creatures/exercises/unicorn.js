3d9116fb4a57225650bd8a840e704b127e6800da
Colby Pearce <crpearce892@gmail.com>
2022-05-18 19:37:26 -0600

    refactored code

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,13 +1,8 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    if (color === undefined) {
-      this.color = 'white';
-    } else {
-      this.color = color;
-    }
+    this.color = color || 'white';
   }
-
   isWhite() {
     if (this.color !== 'white') {
       return false;
@@ -19,3 +14,4 @@ class Unicorn {
 }
 
 module.exports = Unicorn;
+//  npm test mythical-creatures/test/unicorn-test.js
